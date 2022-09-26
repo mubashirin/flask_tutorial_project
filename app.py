7b98436bc103748fff762113a755c7fc9adaa2da
Anton Salamov <gracioso@pm.me>
2022-09-27 02:47:53 +0300

    Fix

--- app.py
+++ app.py
@@ -1,27 +1,66 @@
+import json
+from unicodedata import decimal
+import requests
+from bs4 import BeautifulSoup as BS
 from flask import (
     Flask,
     request,
     render_template,
+    jsonify
 )
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'itbfrdrweq34565iytctexsxt5789obf-te3wxt3s'
 moment = Moment(app)
 
 
-@app.route('/')
+class NewForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    name = None
+    form = NewForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+
     context = {
         'useragent': request.headers.get('User-Agent'),
+        'name': name
     }
-    return render_template('/pages/index.html', current_time=datetime.utcnow(), context=context)
+    return render_template('/pages/index.html', form=form, context=context)
 
 
 @app.route('/user/<name>/')
 def user(name):
     return render_template('/pages/user.html', user=name)
+
+
+@app.route('/tr-usd/')
+def usd_tr():
+    r = requests.get('https://www.kuveytturk.com.tr')
+    html = BS(r.content, 'html.parser')
+    
+    t = html.find('table', {'class': 'table-market'}).find_all('em')
+
+    # print(float(t[1].text))
+    
+    data = {
+        'bank': 'kuveytturk',
+        'usd_tl': t[1].text,
+        'eur_tl': t[5].text,
+    }
+
+    return jsonify(data)
 
 
 @app.errorhandler(404)
